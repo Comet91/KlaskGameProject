c1b859f598c21ef38ff0938034ce80cede401745
Liam Goodchild <liam021002@gmail.com>
2022-05-17 15:10:13 +0100

    Added given classes.

--- KlaskGame/src/KlaskGame.java
+++ KlaskGame/src/KlaskGame.java
@@ -1,6 +1,21 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.geom.*;
+import java.awt.image.*;
+import java.awt.event.*;
+import java.util.*;
+import java.lang.Class;
+import java.lang.reflect.*;
 
-public class KlaskGame {
+public class KlaskGame 
+{
 	
 	
+	
+	public void startGame()
+	{
+		
+		
+	}
 
 }
